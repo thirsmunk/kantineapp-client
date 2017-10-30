b783965efd18764d881e68b2e624d613ba674604
Lasse Munk <lassemunk@mail.com>
2017-10-30 16:14:06 +0100

    added comments for sdk.java, need revising from jesper/morten

--- js/sdk.js
+++ js/sdk.js
@@ -1,4 +1,20 @@
 const SDK = {
     serverURL:  "",
-    request: ()
+    request: (options, callback) => {
+
+        //Array
+        let headers = {};
+
+        //If the object contains headers === true
+        if (options.headers) {
+            //Iterate through the headers array
+            Object.keys(options.headers).forEach((h) => {
+                //Fill out the headers array with the options.headers objects. Using the ternary operator, if the type of options.headers[h] = 'object'
+                //stringify the header and transfer it into the headers array at [h] index, else transfer the options.headers value
+                headers[h] = (typeof options.headers[h] === 'object') ? JSON.stringify(options.headers[h]) : options.headers[h];
+            });
+
+        }
+        
+}
 }
