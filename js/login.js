838aad80a29cbee09b24a15f1b5cd155d70986d1
Lasse Munk <lassemunk@mail.com>
2017-11-07 14:12:22 +0100

    Begun logic of login.js

--- js/login.js
+++ js/login.js
@@ -6,10 +6,22 @@ $(document).ready(() => {
     SDK.Navigation.loadNav();
 
     //On click of the login button
+    $("#login-button").click(() => {
 
-    //Put entered information in boxes into variables
+        //Put entered information in boxes into variables
+        const username = $("#inputUsername").val();
+        const password = $("#inputPassword").val();
 
-    //Send variables as parameters in SDK function
+        //Send variables as parameters in SDK function
+        SDK.LogInOut.logIn(username, password, (err, data) => {
+            //If something exists within the error object and the XML HTTP Request returns a 400 error code do...
+           if (err && err.xhr.status === 401) {
+               //Add error message
+           }
+        });
+
+    });
+
 
     //On success
 
