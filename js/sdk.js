d59c06621ff9094ec62e48d351d7bf6f670a9216
Lasse Munk <lassemunk@mail.com>
2017-12-03 21:47:22 +0100

    modularized checkout and added references where necessary

--- js/sdk.js
+++ js/sdk.js
@@ -332,7 +332,6 @@ const SDK = {
           `);
                 }
 
-                /*MODULARIZE THIS */
                 //If logout is clicked, run the logout function
                 $("#logout-link").click(() => SDK.LogInOut.logOut());
                 cb && cb();
@@ -384,54 +383,6 @@ const SDK = {
                     $("#modal-tbody").children("tr").remove();
                 })
 
-                //If the checkout button is clicked, create the order
-                //make loop that only sends item objects and not count etc
-                $("#checkout-button").click(() => {
-
-                    let windowConfirm = confirm("Are you sure you want to create this order?");
-
-                    if (windowConfirm) {
-
-                        let userId = SDK.Storage.load("user_id");
-                        let basket = SDK.Storage.load("basket");
-                        let orderItems = [];
-
-                        //Add the basket's item contents into another array to be sent to the server
-                        //With parameters element, index, array
-                        basket.forEach((item, i, basket) => {
-                            orderItems.push(basket[i].item);
-                        });
-
-                        //Send it off!
-                        SDK.User.createOrder(userId, orderItems, (err, data) => {
-
-                            //Do something when it's succesful or error
-
-                            if (err && err.xhr.status === 500) {
-
-                                //Something went wrong server side
-                                alert("Server error!");
-
-                            } else if (err) {
-
-                                alert("Couldn't complete order, please try again");
-
-                            } else {
-
-                                //Success
-                                SDK.Storage.remove("basket");
-                                alert("Order completed!");
-                                $("#purchase-modal").modal("toggle");
-                                window.location.href = "orders.html";
-
-                            }
-
-
-                        });
-
-                    }
-
-                });
 
             });
         }
